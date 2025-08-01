3d8d8416a659869f402a9581b9b8d61101ed3e57
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-01 12:22:17 +0300

    main app on api

---
+++ server/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request, Body
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from uvicorn import run
+from api.router import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+if __name__=='__main__':
+    run('main:app', port = 8080, reload=True)
