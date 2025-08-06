535432720c7e05e80610c0749498d9b8a6b8eb6a
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-06 15:07:58 +0300

    add try-except

--- server/test.py
+++ server/test.py
@@ -4,6 +4,9 @@ import requests
 data = {'code':'print(2+4)', 'func_name':'sum','args':[1,2]}
 json_data = json.dumps(data)
 print(json_data)
-response = requests.post('http://127.0.0.1:8080/check', data=json_data, headers={'content-type':'application/json'})
+try:
+    response = requests.post('http://127.0.0.1:8080/check', data=json_data, headers={'content-type':'application/json'})
 # print(json.loads(response.content))
-print(response.json())
+    print(response.json())
+except:
+    print("Удалённый сервер не отвечает")
