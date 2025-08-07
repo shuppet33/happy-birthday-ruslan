86a82f4ecd4e4aa6c8d94c9f542d3487a3e605cd
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-07 13:17:39 +0300

    simple test (only dev)

--- server/test.py
+++ server/test.py
@@ -5,7 +5,7 @@ data = {'code':'print(2+4)', 'func_name':'sum','args':[1,2]}
 json_data = json.dumps(data)
 print(json_data)
 try:
-    response = requests.post('http://127.0.0.1:8080/check', data=json_data, headers={'content-type':'application/json'})
+    response = requests.post('http://195.166.166.82:8000/check', data=json_data, headers={'content-type':'application/json'})
 # print(json.loads(response.content))
     print(response.json())
 except:
